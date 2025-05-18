6f1e658e2842a28e83642b78c5e57f915d92377c
PSCHRIS <97121449+ChrisZP2@users.noreply.github.com>
2025-05-18 19:25:04 -0300

    Update funcoes.h

--- source/bibliotecas/funcoes.h
+++ source/bibliotecas/funcoes.h
@@ -5,9 +5,12 @@ int menu();
 void login(char cpfDigitado[12], char senhaDigitada[61], char nomeLogin[61], char idadeLogin[4], int *logado);
 void selecionar(char selecao[50], char med1[50], char med2[50], char nome_medico[50]);
 void removerQuebraDeLinha(char *str);
-void verificaData(int *hora);
+void verificaData(int dta[3]);
 void verificaHorario(int *escolha);
 void lembrete(int *dia,int hora,char *med);
+void buscar_consulta(const char *nome, const char *cpf);
+void reagendar_consulta(const char *cpf);
+void cancelar_consulta(const char *cpf);
 
 struct dados_paciente // Estrutura para guardar as informações
 {
